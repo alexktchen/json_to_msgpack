829280a6b3e9ebf044e4f7dfcba332e45ad1421c
alex.ktchen <alex.ktchen@kaiostech.com>
2023-07-08 03:41:54 +0800

    add unit test

---
+++ builders/main_test.go
@@ -0,0 +1,30 @@
+package builders
+
+import (
+	"testing"
+)
+
+func TestConvertJSONtoMessagePack(t *testing.T) {
+	// Test JSON data
+	jsonData := []byte(`{
+		"name": "John",
+		"age": 30,
+		"city": "New York",
+		"likes": ["reading", "coding"],
+		"details": {
+			"height": 180,
+			"weight": 75.5
+		}
+	}`)
+
+	// Convert JSON to MessagePack
+	result, err := ConvertJSONtoMessagePack(jsonData)
+	if err != nil {
+		t.Errorf("Error converting JSON to MessagePack: %v", err)
+	}
+
+	// Check if the converted data matches the expected data
+	if result == nil {
+		t.Errorf("result is nil")
+	}
+}
